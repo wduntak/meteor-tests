9ca293a033c0cda97479187e97c55f23727d8d1a
wduntak <wduntak@gmail.com>
2015-02-07 08:56:56 -0500

    Created league helpers

--- client/templates/leagues/leagues_list.js
+++ client/templates/leagues/leagues_list.js
@@ -1,15 +1,18 @@
 var leaguesData = [
 	{
 		name: 'Toronto Masryk Center',
-		address: '300 Cowen st., Toronto, ON'
+		address: '300 Cowen st., Toronto, ON',
+		url: 'http://masryk.com'
 	},
 	{
 		name: 'YMCA Toronto',
-		address: '500 Yonge st., Toronto, ON'
+		address: '500 Yonge st., Toronto, ON',
+		url: 'http://ymcatoronto.com'
 	},
 	{
 		name: 'University of Toronto',
-		address: '1000 University ave., Toronto, ON'
+		address: '1000 University ave., Toronto, ON',
+		url: 'http://uoft.ca'
 	}
 ];
 
