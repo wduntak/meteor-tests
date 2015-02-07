bd595a1445b0d99b5258ad3aa078c3fbb1933ee4
wduntak <wduntak@gmail.com>
2015-02-07 14:21:37 -0500

    Call Leagues collection into leaguesList template

--- client/templates/leagues/leagues_list.js
+++ client/templates/leagues/leagues_list.js
@@ -1,21 +1,5 @@
-var leaguesData = [
-	{
-		name: 'Toronto Masryk Center',
-		address: '300 Cowen st., Toronto, ON',
-		url: 'http://masryk.com'
-	},
-	{
-		name: 'YMCA Toronto',
-		address: '500 Yonge st., Toronto, ON',
-		url: 'http://ymcatoronto.com'
-	},
-	{
-		name: 'University of Toronto',
-		address: '1000 University ave., Toronto, ON',
-		url: 'http://uoft.ca'
-	}
-];
-
 Template.leaguesList.helpers({
-	leagues: leaguesData
+	leagues: function() {
+		return Leagues.find();
+	}
 });
